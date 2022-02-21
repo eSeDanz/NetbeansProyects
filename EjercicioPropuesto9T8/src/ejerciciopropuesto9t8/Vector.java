8ef47c16324f865270edd8a2196c06d8b3a2ce25
Dani Sanz <yellowtraktor@hotmail.com>
2022-02-21 09:14:39 +0100

    Subo Cosas

---
+++ EjercicioPropuesto9T8/src/ejerciciopropuesto9t8/Vector.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciopropuesto9t8;
+
+import java.util.Arrays;
+
+public class Vector {
+
+    private static int[] vector;
+
+    public static void main(String[] args) {
+        vector = crearVector();
+        mostrarNoAlmacenados();
+    }
+
+    public static int[] crearVector() {
+        int[] vector = new int[100];
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * 100 + 1);
+        }
+        java.util.Arrays.sort(vector);
+        System.out.println("Vector Generado: ");
+        System.out.println(Arrays.toString(vector));
+        System.out.println("");
+        return vector;
+    }
+    
+    public static void mostrarNoAlmacenados() {
+        int[] vectoraux= new int[101];
+        for (int i=0; i<vector.length;i++) {
+            vectoraux[vector[i]]++;
+        }
+        String frase="Los números que no han sido almacenados son los siguientes: ";
+        for (int i=1; i<vectoraux.length;i++) {
+            if(vectoraux[i]==0) {
+                frase+=i+" ";
+            }
+        }
+        System.out.println(frase);
+    }
+}

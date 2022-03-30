3e4be62a0e31b4a8492563463ee81221953bd347
Dani Sanz <yellowtraktor@hotmail.com>
2022-03-30 09:49:24 +0200

    Ficheros

--- EjercicioPractico4T12/src/ejerciciopractico4t12/EjercicioPractico4T12.java
+++ EjercicioPractico4T12/src/ejerciciopractico4t12/EjercicioPractico4T12.java
@@ -9,24 +9,24 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class EjercicioPractico4T12 {
 
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Introduce la ruta del fichero a copiar: ");
-        String origen= sc.nextLine();
+        String origen = sc.nextLine();
         System.out.println("Introduce la ruta del fichero destino: ");
-        String destino= sc.nextLine();
-        try{
-            FileInputStream fis=new FileInputStream(origen);
-            FileOutputStream fos=new FileOutputStream(destino);
-            byte[] array= new byte[fis.available()];
+        String destino = sc.nextLine();
+        try {
+            FileInputStream fis = new FileInputStream(origen);
+            FileOutputStream fos = new FileOutputStream(destino);
+            byte[] array = new byte[fis.available()];
             fis.read(array);
             fos.write(array);
             fos.close();
             fis.close();
-        } catch(IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println("Error copiando los ficheros");
         }
     }

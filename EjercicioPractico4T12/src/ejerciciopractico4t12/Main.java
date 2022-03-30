e61800e13252008ee0b40b5bbb1e3943d3f82f1e
Dani Sanz <yellowtraktor@hotmail.com>
2022-03-30 09:38:13 +0200

    Examen 2Evaluacion 2021

---
+++ EjercicioPractico4T12/src/ejerciciopractico4t12/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciopractico4t12;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Introduce la ruta del fichero a copiar: ");
+        String origen= sc.nextLine();
+        System.out.println("Introduce la ruta del fichero destino: ");
+        String destino= sc.nextLine();
+        try{
+            FileInputStream fis=new FileInputStream(origen);
+            FileOutputStream fos=new FileOutputStream(destino);
+            byte[] array= new byte[fis.available()];
+            fis.read(array);
+            fos.write(array);
+            fos.close();
+            fis.close();
+        } catch(IOException ioe) {
+            System.out.println("Error copiando los ficheros");
+        }
+    }
+
+}

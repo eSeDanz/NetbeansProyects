e5154ed6427a72c88bc5c6596e32d1fd3bc3468a
Dani Sanz <yellowtraktor@hotmail.com>
2022-04-05 14:00:36 +0200

    prueba

--- EjercicioPractico5T12/src/ejerciciopractico5t12/EjercicioPractico5T12.java
+++ EjercicioPractico5T12/src/ejerciciopractico5t12/EjercicioPractico5T12.java
@@ -4,13 +4,32 @@
  */
 package ejerciciopractico5t12;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class EjercicioPractico5T12 {
 
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String hola;
-                
+        System.out.print("Introduce la cantidad de numeros enteros y positivos que quieres generar: ");
+        int cantidad = sc.nextInt();
+        System.out.print("Introduce la ruta del fichero de destino: ");
+        sc.nextLine();
+        String destino = sc.next();
+        try {
+            FileOutputStream fos = new FileOutputStream(destino, true);
+            DataOutputStream dos = new DataOutputStream(fos);
+            FileInputStream fis = new FileInputStream(destino);
+            DataInputStream dis = new DataInputStream(fis);
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+
     }
-    
+
 }

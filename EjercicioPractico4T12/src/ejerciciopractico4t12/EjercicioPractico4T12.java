ff5fe38beec02c71b8f54c079a99c7c671c79263
Dani Sanz <yellowtraktor@hotmail.com>
2022-04-05 12:51:10 +0200

    Merge origin/main into main

--- EjercicioPractico4T12/src/ejerciciopractico4t12/EjercicioPractico4T12.java
+++ EjercicioPractico4T12/src/ejerciciopractico4t12/EjercicioPractico4T12.java
@@ -4,6 +4,7 @@
  */
 package ejerciciopractico4t12;
 
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,21 +15,20 @@ public class EjercicioPractico4T12 {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Introduce la ruta del fichero a copiar: ");
-        String origen = sc.nextLine();
-        System.out.println("Introduce la ruta del fichero destino: ");
-        String destino = sc.nextLine();
-        try {
-            FileInputStream fis = new FileInputStream(origen);
-            FileOutputStream fos = new FileOutputStream(destino);
-            byte[] array = new byte[fis.available()];
+        System.out.print("Introduce la ruta del fichero a copiar: ");
+        String origen= sc.nextLine();
+        System.out.print("Introduce la ruta del fichero destino: ");
+        String destino= sc.nextLine();
+        try{
+            FileInputStream fis=new FileInputStream(origen);
+            FileOutputStream fos=new FileOutputStream(destino);
+            byte[] array= new byte[fis.available()];
             fis.read(array);
             fos.write(array);
             fos.close();
             fis.close();
-        } catch (IOException ioe) {
+        } catch(IOException ioe) {
             System.out.println("Error copiando los ficheros");
         }
     }
-
 }

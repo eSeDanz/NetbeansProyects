b4bc5b09b625767a5ee7ecd8c4eec41832bbbe7a
Dani Sanz <yellowtraktor@hotmail.com>
2022-04-06 09:55:31 +0200

    Ejercicio5T12

--- EjercicioPractico5T12/src/ejerciciopractico5t12/EjercicioPractico5T12.java
+++ EjercicioPractico5T12/src/ejerciciopractico5t12/EjercicioPractico5T12.java
@@ -6,7 +6,9 @@ package ejerciciopractico5t12;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
@@ -15,7 +17,7 @@ public class EjercicioPractico5T12 {
 
     private static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.print("Introduce la cantidad de numeros enteros y positivos que quieres generar: ");
         int cantidad = sc.nextInt();
         System.out.print("Introduce la ruta del fichero de destino: ");
@@ -24,12 +26,22 @@ public class EjercicioPractico5T12 {
         try {
             FileOutputStream fos = new FileOutputStream(destino, true);
             DataOutputStream dos = new DataOutputStream(fos);
-            FileInputStream fis = new FileInputStream(destino);
-            DataInputStream dis = new DataInputStream(fis);
+            for (int i = 0; i < cantidad; i++) {
+                int random = (int) (Math.random() * 101 + 0);
+                dos.writeInt(random);
+            }
         } catch (IOException ioe) {
             System.out.println(ioe);
         }
-
+        try {
+            FileInputStream fis = new FileInputStream(destino);
+            DataInputStream dis = new DataInputStream(fis);
+            while (true) {
+                System.out.println(dis.readInt());
+            }
+        } catch (EOFException e) {
+            System.out.println("Fin de Fichero");
+        }
     }
 
 }

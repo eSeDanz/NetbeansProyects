b18c716ae9a4ab48f6ef389453d5f24a22bb0fe6
Dani Sanz <yellowtraktor@hotmail.com>
2022-05-06 12:11:57 +0200

    Ejercicio5T12

--- Tema11EjercicioPropuesto2/src/tema11ejerciciopropuesto2/Main.java
+++ Tema11EjercicioPropuesto2/src/tema11ejerciciopropuesto2/Main.java
@@ -4,38 +4,53 @@
  */
 package tema11ejerciciopropuesto2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class Main {
-    public static Scanner sc=new Scanner(System.in);
-    
+
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int pos;
-        System.out.print("Introduce el fibonacci que quieres calcular: ");
-        pos= sc.nextInt();
-        for(int i=1; i<=pos; i++){
-            System.out.print(fibonacci_iterativo(i) + " ");
+        try {
+            int pos;
+            System.out.print("Introduce el fibonacci que quieres calcular: ");
+            pos = sc.nextInt();
+            for (int i = 1; i <= pos; i++) {
+                System.out.print(fibonacci_iterativo(i) + " ");
+            }
+            System.out.println("");
+            for (int i = 1; i <= pos; i++) {
+                System.out.print(fibonacci_recursivo(i) + " ");
+            }
+        } catch (InputMismatchException f1) {
+            System.out.println("Error en el tipo de datos introducidos");
         }
-       // fibonacci_recursivo(5);
     }
-    
-    public static void fibonacci_recursivo(int posicion){
-        
-    }
-    
-    public static int fibonacci_iterativo(int posicion){
-        int ultimo=1, penultimo=0, fib=0;
-        if(posicion==1){
+
+    public static int fibonacci_recursivo(int posicion) {
+        if (posicion == 1) {
             return 0;
-        } else if(posicion==2){
+        } else if (posicion == 2) {
+            return 1;
+        } else {
+            return fibonacci_recursivo(posicion - 1) + fibonacci_recursivo(posicion - 2);
+        }
+    }
+
+    public static int fibonacci_iterativo(int posicion) {
+        int ultimo = 1, penultimo = 0, fib = 0;
+        if (posicion == 1) {
+            return 0;
+        } else if (posicion == 2) {
             return 1;
         }
-        
-        for(int i=3; i<=posicion; i++){
-            fib=ultimo+penultimo;
-            penultimo=ultimo;
-            ultimo=fib;
-        } return fib;
+
+        for (int i = 3; i <= posicion; i++) {
+            fib = ultimo + penultimo;
+            penultimo = ultimo;
+            ultimo = fib;
+        }
+        return fib;
     }
 }

ffea3328d3c4d2f2d425616f97a7aa9581655df1
Dani Sanz <yellowtraktor@hotmail.com>
2022-05-06 10:27:09 +0200

    Ejercicio5T12

---
+++ Tema11EjercicioPropuesto3/src/tema11ejerciciopropuesto3/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tema11ejerciciopropuesto3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int lado;
+        System.out.print("Introduce el tamaño de lado del cuadrado: ");
+        lado = sc.nextInt();
+        for(int i=1; i<=lado;i++){
+            for (int j = 1; j <= lado; j++) {
+                System.out.print(cuadradoRecursivo(i, j) + "\t");
+            } System.out.println("");
+        }
+    }
+
+    public static int cuadradoRecursivo(int fila, int columna){
+        if(fila==1 || columna==1){
+            return 1;
+        } else {
+            return cuadradoRecursivo(fila-1,columna)+cuadradoRecursivo(fila,columna-1);
+        }
+    }
+}

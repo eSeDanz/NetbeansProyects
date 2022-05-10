80717e1af073362d738d10ea820662df48715eee
Dani Sanz <yellowtraktor@hotmail.com>
2022-05-10 13:08:57 +0200

    Ejercicio5T11

--- Tema11EjercicioPropuesto6/src/tema11ejerciciopropuesto6/transformabase.java
+++ Tema11EjercicioPropuesto6/src/tema11ejerciciopropuesto6/transformabase.java
@@ -4,6 +4,7 @@
  */
 package tema11ejerciciopropuesto6;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class transformabase {
@@ -11,24 +12,32 @@ public class transformabase {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int num, base;
-        System.out.print("Introduce el número que quieres convertir: ");
-        num = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Introduce la base a la que quieres convertirlo(2,8,16): ");
-        base = sc.nextInt();
-        sc.nextLine();
-        while (base != 2 && base != 8 && base != 16) {
-            System.out.println("La base introducida es incorrecta");
-            System.out.print("Vuelva a introducir la base(2,8,16): ");
+        try {
+            int num, base;
+            System.out.print("Introduce el número que quieres convertir: ");
+            num = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Introduce la base a la que quieres convertirlo(2,8,16): ");
             base = sc.nextInt();
-        }
-        if (base == 2) {
-            decimalBinario(num);
-        } else if (base == 8) {
-            decimalOctal(num);
-        } else {
-            decimalHexadecimal(num);
+            sc.nextLine();
+            while (base != 2 && base != 8 && base != 16) {
+                System.out.println("La base introducida es incorrecta");
+                System.out.print("Vuelva a introducir la base(2,8,16): ");
+                base = sc.nextInt();
+                sc.nextLine();
+            }
+            if (base == 2) {
+                System.out.print(num + " en binario es igual a ");
+                decimalBinario(num);
+            } else if (base == 8) {
+                System.out.print(num + " en octal es igual a ");
+                decimalOctal(num);
+            } else {
+                System.out.print(num + " en hexadecimal es igual a ");
+                decimalHexadecimal(num);
+            }
+        } catch (InputMismatchException IME) {
+            System.out.println("Error en el tipo de datos");
         }
     }
 
@@ -53,27 +62,27 @@ public class transformabase {
     public static void decimalHexadecimal(int num) {
         if (num < 16) {
             System.out.print(HexToString(num));
-        }else{
-            decimalHexadecimal(num/16);
-            System.out.print(HexToString(num%16));
+        } else {
+            decimalHexadecimal(num / 16);
+            System.out.print(HexToString(num % 16));
         }
     }
-    
-    public static String HexToString(int num){
+
+    public static String HexToString(int num) {
         if (num == 10) {
-                return "A";
-            } else if (num == 11) {
-                return "B";
-            } else if (num == 12) {
-                return "C";
-            } else if (num == 13) {
-                return "D";
-            } else if (num == 14) {
-                return "E";
-            } else if (num == 15) {
-                return "F";
-            } else {
-                return ""+num;
-            }
+            return "A";
+        } else if (num == 11) {
+            return "B";
+        } else if (num == 12) {
+            return "C";
+        } else if (num == 13) {
+            return "D";
+        } else if (num == 14) {
+            return "E";
+        } else if (num == 15) {
+            return "F";
+        } else {
+            return "" + num;
+        }
     }
 }
